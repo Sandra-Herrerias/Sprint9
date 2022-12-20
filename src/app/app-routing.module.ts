ada2fcab80d9d2ddbcace22659e96ba23cbe8003
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-12-20 20:49:20 +0100

    navbar + initial routing + categories +home comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'categories',
+  component: CategoriesComponent
+},
+{
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+},
+{
+  path: '**',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    CategoriesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
