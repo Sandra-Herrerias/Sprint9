d50c09d6cc31b43e8af7a22f1efa76f97e27aa6b
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2022-12-29 20:26:11 +0100

    register updated

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  registerForm!: FormGroup;
+
+  submitted = false;
+  message: string = "";
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UsersService) { }
+
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+
+
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.valid) {
+      let existsUser = this.userService.getUsersStored().some(user => {
+        let currentUser = user.username;
+        let formUser = this.registerForm.value.username;
+        return currentUser == formUser;
+      });
+
+      if (existsUser) {
+        alert("Insert new username, this one already exists");
+      } else {
+        this.userService.register(this.registerForm.value);
+        this.registerForm.reset();
+      }
+
+    } else {
+      return;
+    }
+  }
 }
+
+
