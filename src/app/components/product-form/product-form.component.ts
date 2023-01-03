5b92091ba62508b375c75bcd13e860333ceb2663
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2023-01-03 22:10:15 +0100

    product form updated

--- src/app/components/product-form/product-form.component.ts
+++ src/app/components/product-form/product-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
@@ -7,9 +8,30 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductFormComponent implements OnInit{
 
-  @Input() productInForm!: '';
+  @Input() productInForm!: any;
+  prodForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder){
+  }
 
   ngOnInit():void{
+    console.log(JSON.stringify(this.productInForm));
+    console.log(typeof(this.productInForm.title));
+    console.log(this.productInForm.id);
+    console.log(this.productInForm.title);
 
+    
+
+    this.prodForm = this.formBuilder.group({
+      id:this.productInForm.id,
+      title:this.productInForm.title
+    });
+  }
+
+  get f() { return this.prodForm.controls; }
+
+  registerFormSubmit(value:any) {
+    console.log(value);
   }
 }
