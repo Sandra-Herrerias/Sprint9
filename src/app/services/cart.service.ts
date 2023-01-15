bb311784f3b8c5804ba9a5840478eaf45f514022
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2023-01-15 20:58:11 +0100

    empty functions created

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
+  //llista de productes seleccionats
+  //
+
   constructor() { }
 
 
-  addToCart(){
-    
+  addToCart(product:Product){
+    //afegir un producte nou a la llista
   }
+
+  existsProduct(){
+    //finds a product in list product
+  }
+
+  sendOrder(){
+    //print console + alert
+  }
+  
 }
