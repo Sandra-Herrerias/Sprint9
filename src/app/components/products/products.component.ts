282f0fb59dfb7bad0bb6543aaa736f6d21497e6a
SandraHerrerias <s.herreriasrodriguez@gmail.com>
2023-01-22 11:46:20 +0100

    products creates admin role

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/models/product';
+import { User } from 'src/app/models/user';
 import { CartService } from 'src/app/services/cart.service';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { ProductsService } from 'src/app/services/products.service';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-products',
@@ -18,6 +20,8 @@ export class ProductsComponent implements OnInit {
   ipp!: number;
   cp!: number;
   showByCategory!: boolean;
+  adminExists: boolean = false;
+  usersInLocalStorage!: Array<User>;
 
   /** CART VARIABLES */
   //Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
@@ -29,7 +33,8 @@ export class ProductsComponent implements OnInit {
     private prodService: ProductsService,
     private route: ActivatedRoute,
     private cartProdsService: CartService,
-    private router: Router) {
+    private router: Router,
+    private userService: UsersService) {
     this.ipp = 10;
     this.cp = 1;
   }
@@ -44,6 +49,29 @@ export class ProductsComponent implements OnInit {
       this.getProductsByCategory();
     } else {
       this.listProducts();
+    }
+
+    this.usersInLocalStorage = this.userService.getUsersStored();
+
+    if (this.usersInLocalStorage != null && this.usersInLocalStorage != undefined) {
+      this.usersInLocalStorage.find((user: User) => {
+        if (user.role === "admin") {
+          this.adminExists = true;
+        }
+      });
+    }
+
+    if (!this.adminExists) {
+      const mainUser: User = {
+        id: 1,
+        username: 'admin@admin.com',
+        password: '123456',
+        firstName: 'admin',
+        lastName: 'admin',
+        role: 'admin'
+      };
+
+      this.userService.register(mainUser);
     }
   }
 
@@ -65,7 +93,7 @@ export class ProductsComponent implements OnInit {
   }
 
   getProdDetail(product: any) {
-    this.router.navigate(['/prodDetail'], {queryParams: {productId: product.id}});
+    this.router.navigate(['/prodDetail'], { queryParams: { productId: product.id } });
   }
 
   /** CART FUNCTIONS */
